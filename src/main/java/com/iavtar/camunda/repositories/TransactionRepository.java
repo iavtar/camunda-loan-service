adc12598dc89cfe73f9de6c8eba797e4df868a14
iavtar <indra.avatarsingh@gmail.com>
2023-02-25 19:47:17 +0530

    added transaction repository

---
+++ src/main/java/com/iavtar/camunda/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.iavtar.camunda.repositories;
+
+import com.iavtar.camunda.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+}
